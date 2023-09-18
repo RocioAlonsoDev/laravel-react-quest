e904e6d0ba5872119bf68f9121adff3f6a45dbb9
Rocio <rocioalonso.dev@gmail.com>
2023-09-18 22:48:25 +0200

    created button atom component

--- react/src/views/home/Home.jsx
+++ react/src/views/home/Home.jsx
@@ -4,14 +4,17 @@ import EventAtom from "../../components/atoms/eventAtom/EventAtom"
 
 export default function Home() {
   const { events } = UseStateContext();
-  console.log(events);
+  const onDeleteClick = () => {
+    console.log("On Delete Click")
+  }
+
   return (
     <>
     
       <PageComponent title='Inicio'>
         <div className="grid grid-cols-1 gap-5 sm:grid-cols-2 md:grid-cols-3">
         {events.map(event => (
-          <EventAtom event={event} key={event.id}></EventAtom>
+          <EventAtom event={event} key={event.id} onDeleteClick={onDeleteClick}></EventAtom>
         ))}
         </div>
       </PageComponent>
