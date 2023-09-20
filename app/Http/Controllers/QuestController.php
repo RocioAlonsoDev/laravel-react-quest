9b3409f1021e049d84f194408376dee599b7a1d1
Rocio <rocioalonso.dev@gmail.com>
2023-09-20 05:13:36 +0200

    created quest models and migrations

---
+++ app/Http/Controllers/QuestController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Quest;
+use App\Http\Requests\StoreQuestRequest;
+use App\Http\Requests\UpdateQuestRequest;
+
+class QuestController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    public function store(StoreQuestRequest $request)
+    {
+        //
+    }
+
+    public function show(Quest $quest)
+    {
+        //
+    }
+
+    public function update(UpdateQuestRequest $request, Quest $quest)
+    {
+        //
+    }
+
+    public function destroy(Quest $quest)
+    {
+        //
+    }
+}
---
+++ database/migrations/2023_09_20_024026_create_quests_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations. 
+     * title
+     * description
+     * banner img
+     * requisites
+     * date
+     * location
+     * creator_id
+     */
+    public function up(): void
+    {
+        Schema::create('quests', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(\App\Models\User::class, 'creator_id');
+            $table->string('title',255);
+            $table->longText('description')->nullable();
+            $table->string('banner_img',255);
+            $table->text('requisites',255);
+            $table->timestamp('date')->nullable();
+            $table->string('location',255);
+            $table->integer('max_users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('quests');
+    }
+};
